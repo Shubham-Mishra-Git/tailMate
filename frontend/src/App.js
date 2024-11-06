2718de8b09d39884ed91b017d6c6340adde4a938
aditya sharma <adityasharma42072@gmail.com>
2024-11-06 18:40:14 +0530

    initial commit just basic setup of react and express

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="name bg-red-500">hello there everyone </div>
+    </div>
+  );
+}
+
+export default App;
