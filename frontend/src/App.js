2abfe359fd5e2471067a84bd58d726bc004abc33
Shalini <abcshalini1245@gmail.com>
2024-11-10 11:09:20 +0530

    changes here

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="name bg-red-500">hello there everyone </div>
+      <div className="name bg-red-500">hello there everyone.....how are you </div>
     </div>
   );
 }
